e714ae13e72b36dd34789bc1d7a36cc01df3934c
Busiso <busisondumo1@gmail.com>
2024-04-03 04:52:45 +0200

    (Fix): fix possible errors

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -16,7 +16,7 @@ def custom_function(a, b):
             if i > a:
                 raise Exception('Too far')
             result += (a ** b) / i
-        except:
+        except ValueError:
             result += a + b
             break
 
